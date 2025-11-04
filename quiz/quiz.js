b83b2be6ed4f9f012d4367ed75d6591a7f949a54
Drashti <drashtidevani47@gmail.com>
2025-11-04 10:07:59 +0530

    quiz

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -25,6 +25,8 @@ var question = document.querySelector('.question');
 var optionsContainer = document.querySelector('.options-container')
 var optionsButtons = document.querySelectorAll('.options-container button')
 var resultScore = document.getElementById('result-score');
+var resultTable = document.querySelector('.result-table table tbody');
+
 
 var currentQuestionIndex = 0;
 var userAnswers = [];
@@ -68,11 +70,30 @@ function getQueryParams() {
     console.log(params);
     return params;
 }
+const queryParams = getQueryParams();
+
 
 if (resultScore) {
     const queryParams = getQueryParams();
     const score = queryParams['score'];
     const total = queryParams['total'];
     resultScore.innerHTML = `you got ${score}/${total} marks`;
+}
+if (resultTable) {
+    let resultAnswers = queryParams.answers.split(',')
+
+    let tableBody = "";
+
+    questions.forEach((item, index) => {
+        tableBody += `
+        <tr>
+            <td>${item.question}</td>
+            <td>${item.options[item.answer]}</td>
+            <td>${item.options[resultAnswers[index]]}</td>
+        </tr>
+    `
+    });
+
+    resultTable.innerHTML = tableBody;
 }
 
